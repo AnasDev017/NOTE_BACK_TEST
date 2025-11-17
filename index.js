694e4d17027e7eb48b90bceefdbd0d23698cccf9
mypersonalid2472-lab · he/him <mypersonalid2472@gmail.com>
2025-11-17 10:16:31 +0500

    code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ DbCon()
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'  
+    origin: 'https://mern-note-app-frontend-zeta.vercel.app'  
 }));
 app.use(cookieParser())
 app.use(express.json())
